95c1cc340b9e3db5d1c4af6e0f0005fdd419e8b5
Matthew Varga <matthew.l.varga@gmail.com>
2019-06-10 14:19:18 -0400

    fixed bug with item collision, so now works for players with height 96 as well as 48

--- src/player/utils/utils/_handleItemCollision.js
+++ src/player/utils/utils/_handleItemCollision.js
@@ -2,18 +2,20 @@ import Player from "../../Player";
 import Items from "../../../items/Items";
 import collides from "../../../utils/collisionDetection/collides";
 import _handleItemCollisionByType from "./_handleItemCollisionByType";
+import gameConfig from "../../../resources/config.json";
 
 export default function _handleItemCollision() {
-    let playerColL = Math.floor(Player.getGlobalX()/Player.getWidth());
-    let playerColR = Math.ceil(Player.getGlobalX()/Player.getWidth());
+    let playerColL = Math.floor(Player.getGlobalX()/gameConfig.map.tiles.width);
+    let playerColR = Math.ceil(Player.getGlobalX()/gameConfig.map.tiles.width);
 
-    let playerRowT = Math.floor(Player.getGlobalY()/Player.getHeight());
-    let playerRowB = Math.ceil(Player.getGlobalY()/Player.getHeight());
+    let playerRowT = Math.floor(Player.getGlobalY()/gameConfig.map.tiles.height);
+    let playerRowB = playerRowT + Player.getHeight();
 
 
     if(Items.getNumItems()) {
         for(let c = playerColL; c <= playerColR; c++) {
             for(let r = playerRowT; r <= playerRowB; r++) {
+
                 let item = Items.getItem(c+1, r+1); // need to add 1 to each, because items count startin from 1
                 
                 // if an item exists on the tile, check if player actually collides with its hitbox or not
